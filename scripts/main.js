b58a377bd6ac1c81c57b9a3bdbdfedf19119d0db
brunastockett7 <bruna__stockett@outlook.com>
2025-05-16 10:20:24 -0800

    final js

---
+++ scripts/main.js
@@ -0,0 +1,8 @@
+
+/* global document */
+
+// Set the current year
+document.getElementById("year").textContent = new Date().getFullYear();
+
+// Set the last modified date
+document.getElementById("lastModified").textContent = document.lastModified;
